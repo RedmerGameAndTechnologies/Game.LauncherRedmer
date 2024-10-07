59b0750f64ebe8abf2a480a89b9d8f202ac4c067
Michael <michaelvoov@yandex.ru>
2024-10-07 16:03:29 +0500

    Update

--- LauncherLes1/LauncherLes1/View/Pages/Game/DefenderRatPage.xaml.cs
+++ LauncherLes1/LauncherLes1/View/Pages/Game/DefenderRatPage.xaml.cs
@@ -25,5 +25,10 @@ namespace LauncherLes1.View {
         {
             InitializeComponent();
         }
+
+        private void ButtonCancelDownloadApp(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- LauncherLes1/LauncherLes1/View/Pages/HomePage.xaml.cs
+++ LauncherLes1/LauncherLes1/View/Pages/HomePage.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace LauncherLes1.View.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для HomePage.xaml
-    /// </summary>
     public partial class HomePage : Page
     {
         public HomePage()
